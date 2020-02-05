57750b0a3ab774c55ddec117082ee453c1605a61
Carl Njoku <flavoursoft@yahoo.com>
2020-02-05 19:03:46 +0100

    created room route

--- socketio_server.py
+++ socketio_server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify, render_template
-from flask_socketio import SocketIO, emit,send
+from flask_socketio import SocketIO, emit,send, join_room
 from flask_cors import CORS
+from db import database
+from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
@@ -8,7 +10,7 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 socketio.init_app(app, cors_allowed_origins="*")
 
-users = {}
+users = []
 @app.route('/')
 def index():
     return 'Hello world !'
@@ -20,18 +22,36 @@ def message(data):
 
 @socketio.on('username', namespace='/chat')
 def get_username(username):
-    users[username] = request.sid
+    users.append({username: request.sid})
     print(users)
 
 @socketio.on('send_message', namespace='/chat')
 def send_message(payload):
-    recipient_sid = payload[users['username']]
+    recipient_sid = request.sid
     message = payload['message']
     room = recipient_sid
-    emit('new_message', message)
-    print('This should be the room' +room)
+    #emit('new_message', message, room, broadcast=True)
+    #print('This should be the room' +room)
+
+    users.append(request.sid)
+
+    #room = recipient_sid
+    #join_room(room)
+    
+    emit('new_message', message, room=recipient_sid)
 
 
+@app.route('/create_room')
+def create_room():
+    try:
+        req_data = request.get_json()
+
+        req_data['_id'] = str(ObjectId())
+        x = database["rooms"].insert_one(req_data)
+        return jsonify({"result": "room successfully created"})
+
+    except Exception as e:
+        return jsonify({"result": {"error_msg": str(e)}})
 
 
 
